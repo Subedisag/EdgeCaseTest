5c8b0ace3f40fabf76a346fbde3843ce87e4a9d0
Subedisag <122575733+Subedisag@users.noreply.github.com>
2023-06-01 15:08:23 -0400

    Create ctrlexp.cpp

---
+++ Anomaly/ctrlexp.cpp
@@ -0,0 +1,21 @@
+// this is to test the ctrl expressio with left operator
+
+if (temp = (unsigned(unbase64[src[0]]) << 18)) {}
+
+
+if (!src[0] || !src[1] || !src[2] ||
+          (temp = ((unsigned(unbase64[src[0]]) << 18) |
+                   (unsigned(unbase64[src[1]]) << 12) |
+                   (unsigned(unbase64[src[2]]) << 6) |
+                   (unsigned(unbase64[src[3]])))) & 0x80000000)
+  
+  
+    if (!(ptr = (const char **)alloc->Alloc(
+            (my_args.size() + *argc + 1 + args_sep) * sizeof(char *))))
+    goto err;
+  res = ptr;
+
+
+
+if ((data[length - 1 - j] & (1 << k)) != 0)
+            break;
